9dbae86394009ae4b76fb519437c0d7319c35ab9
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-03-18 03:46:38 -0300

    Mejora Build Gradle Engine

--- engine/build.gradle.kts
+++ engine/build.gradle.kts
@@ -1,10 +1,7 @@
-val kotlin_version: String by project
-val logback_version: String by project
-
 plugins {
-    kotlin("jvm") version "2.3.0"
-    id("io.ktor.plugin") version "3.4.1"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0"
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+    id("io.ktor.plugin") version "2.3.9"
 }
 
 group = "com.vueltaf1nal"
@@ -29,7 +26,6 @@ dependencies {
     implementation("io.ktor:ktor-server-call-id")
     implementation("io.ktor:ktor-server-content-negotiation")
     implementation("io.ktor:ktor-serialization-kotlinx-json")
-    implementation("io.github.flaxoos:ktor-server-rate-limiting:2.2.1")
     implementation("io.ktor:ktor-server-thymeleaf")
     implementation("io.ktor:ktor-server-csrf")
     implementation("io.ktor:ktor-server-auth")
@@ -38,7 +34,6 @@ dependencies {
     implementation("io.ktor:ktor-server-auth-jwt")
     implementation("org.openfolder:kotlin-asyncapi-ktor:3.1.3")
     implementation("io.ktor:ktor-server-netty")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
     testImplementation("io.ktor:ktor-server-test-host")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
