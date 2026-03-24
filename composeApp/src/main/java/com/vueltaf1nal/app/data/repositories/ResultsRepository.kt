c671c51670377e355c4ace0f4de45aaccd593e9a
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-03-24 17:41:03 -0300

    Saco Capa Repository Composse

--- composeApp/src/main/java/com/vueltaf1nal/app/data/repositories/ResultsRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.vueltaf1nal.app.data.repositories
-
-import com.vueltaf1nal.app.domain.models.DriverResult
-
-interface ResultsRepository {
-    suspend fun getDriverResults(): List<DriverResult>
-}
--- composeApp/src/main/java/com/vueltaf1nal/app/data/repositories/TopSpeedsRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.vueltaf1nal.app.data.repositories
-
-import com.vueltaf1nal.app.domain.models.MaxSpeed
-
-interface TopSpeedsRepository {
-    suspend fun getTopSpeeds(): List<MaxSpeed>
-}
