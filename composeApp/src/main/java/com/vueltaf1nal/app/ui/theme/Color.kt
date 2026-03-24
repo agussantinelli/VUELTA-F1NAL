26ec9485d1834e80658ab21423fa7f213c7d9033
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-03-24 00:29:15 -0300

    Agrego Colores a la Paleta

--- composeApp/src/main/java/com/vueltaf1nal/app/ui/theme/Color.kt
+++ composeApp/src/main/java/com/vueltaf1nal/app/ui/theme/Color.kt
@@ -8,6 +8,10 @@ val CircuitNavy = Color(0xFF0B1221)
 val SpeedYellow = Color(0xFFFFCC00)
 val AeroSilver = Color(0xFFD1D1D1)
 
+// New Blue Tones
+val DeepNavy = Color(0xFF050814)
+val BrightNavy = Color(0xFF1E3A5F)
+
 // UI Accent Colors
 val AsphaltBlack = Color(0xFF0B0B0B)
 val CardDark = Color(0xFF1A1A1A)
