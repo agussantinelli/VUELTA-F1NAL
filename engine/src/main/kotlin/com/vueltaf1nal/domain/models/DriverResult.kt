babb9468ddd7a39fdde1a4b8e8c88833d4221273
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-03-24 03:05:22 -0300

    Armo Models Parciales

---
+++ engine/src/main/kotlin/com/vueltaf1nal/domain/models/DriverResult.kt
@@ -0,0 +1,8 @@
+package com.vueltaf1nal.domain.models
+
+data class DriverResult(
+    val position: Int,
+    val driverName: String,
+    val teamName: String,
+    val gap: String
+)
---
+++ engine/src/main/kotlin/com/vueltaf1nal/domain/models/MaxSpeed.kt
@@ -0,0 +1,7 @@
+package com.vueltaf1nal.domain.models
+
+data class MaxSpeed(
+    val driverName: String,
+    val topSpeed: Double,
+    val lap: Int
+)
