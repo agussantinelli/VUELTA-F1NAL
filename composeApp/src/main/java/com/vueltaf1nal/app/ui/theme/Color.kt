198382c0e633bc7d2617d41570a20e9b6bb6dfdb
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-03-24 00:19:19 -0300

    Armo Colores Bases

---
+++ composeApp/src/main/java/com/vueltaf1nal/app/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.vueltaf1nal.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Brand Colors from Official Logo
+val RacingRed = Color(0xFFE10600)
+val CircuitNavy = Color(0xFF0B1221)
+val SpeedYellow = Color(0xFFFFCC00)
+val AeroSilver = Color(0xFFD1D1D1)
+
+// UI Accent Colors
+val AsphaltBlack = Color(0xFF0B0B0B)
+val CardDark = Color(0xFF1A1A1A)
+val TelemetryGreen = Color(0xFF00FF00)
+val DividerGray = Color(0xFF333333)
+val TextGray = Color(0xFF808080)
