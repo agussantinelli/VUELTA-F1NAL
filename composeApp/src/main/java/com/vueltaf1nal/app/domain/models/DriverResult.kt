9999224d2c8eefd6c9347ca0d8483d2146bbe376
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-03-24 12:13:26 -0300

    Armo Modelos Iniciales

---
+++ composeApp/src/main/java/com/vueltaf1nal/app/domain/models/DriverResult.kt
@@ -0,0 +1,8 @@
+package com.vueltaf1nal.app.domain.models
+
+data class DriverResult(
+    val position: Int,
+    val name: String,
+    val team: String,
+    val time: String
+)
---
+++ composeApp/src/main/java/com/vueltaf1nal/app/domain/models/MaxSpeed.kt
@@ -0,0 +1,7 @@
+package com.vueltaf1nal.app.domain.models
+
+data class MaxSpeed(
+    val driverName: String,
+    val topSpeed: Double,
+    val lap: Int
+)
